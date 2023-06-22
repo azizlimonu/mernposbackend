10bf92a5d6c818c37e98565f470a142799c843a9
Aziz Limonu <azizlimonu@gmail.com>
2023-06-22 22:46:37 +0800

    + user model,controller,routes

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const dbConnect = require('./db/dbConnect');
 const app = express();
 dotenv.config();
 
+// routes
+const userRoutes = require("./routes/userRoutes");
+
+app.use(express.json());
+
+app.get("/", (req, res) => res.status(200).send("API OKE BOS"));
+
+// routes API
+app.use("/api/users/", userRoutes);
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, async () => {
